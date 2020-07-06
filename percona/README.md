62587de0bb9ecb3387661aba28be487309ba742c
JJ Clements <clementsjj@gmail.com>
2020-07-06 19:41:17 -0400

    add sql commands

--- percona/README.md
+++ percona/README.md
@@ -1,6 +1,10 @@
 # Prcona XtraDB
 
-curl -O https://raw.githubusercontent.com/clementsjj/configs/master/percona/percona_install_node1.sh
+curl -O https://raw.githubusercontent.com/clementsjj/configs/master/percona_install_node1.sh
+curl -O https://raw.githubusercontent.com/clementsjj/configs/master/percona_install_node2.sh
+curl -O https://raw.githubusercontent.com/clementsjj/configs/master/percona_install_wildcard.sh
+
+sed -i 's/^wsrep_cluster_address=.\*/wsrep_cluster_address=gcomm:\/\/172.31.62.34,172.31.44.133,172.31.35.45/' /etc/mysql/my.cnf
 
 ###### Download mysql config
 
---
+++ sqlstart.sql
@@ -0,0 +1,88 @@
+# show databases;
+# show schemas;
+# create database jj;
+# create schema jj;
+# drop schema jj;
+
+#SHOW VARIABLES LIKE 'auto_inc%'; 
+#SET @@auto_increment_increment=1;
+
+SELECT @@pxc_strict_mode;
+
+CREATE schema devopstest;
+USE devopstest;
+SELECT DATABASE();
+
+#######################
+# Create People Table #
+#######################
+CREATE TABLE People (
+    PersonID INT NOT NULL AUTO_INCREMENT,
+    FName VARCHAR(50),
+    Age INT,
+    PRIMARY KEY (PersonID)
+);
+SHOW TABLES;
+DESCRIBE People;
+
+INSERT INTO People(FName, Age) VALUES ('Alan', 30), ('Betty', 25), ('Christy', 35), ('Dave', 45), ('Elmo', 40);
+SELECT * FROM People;
+
+
+######################
+# Create Bikes Table #
+######################
+CREATE TABLE Bikes (
+    BikeMake VARCHAR(50) NOT NULL,
+    Color VARCHAR(50) NOT NULL,
+    IDPerson int,
+    FOREIGN KEY (IDPerson) REFERENCES People(PersonID)
+);
+SHOW TABLES;
+DESCRIBE People;
+# Need to add an ID column so that we can have a primary key
+# Otherwise percona will throw an error saying everything needs a primary key
+ALTER TABLE Bikes ADD COLUMN `id` int(10);
+# Assign the new column to be the primary key
+ALTER TABLE Bikes MODIFY COLUMN `id` INT(10) UNSIGNED PRIMARY KEY AUTO_INCREMENT;
+
+ALTER TABLE Bikes ADD PRIMARY KEY(id)
+
+INSERT INTO Bikes(BikeMake, Color, IDPerson) VALUES ('Trek', 'Red', 3);
+INSERT INTO Bikes(BikeMake, Color, IDPerson) VALUES ('Spin', 'Black', 6),('Centurion', 'Orange', 9),('Specialized', 'Purple', 12),('Cannondale', 'Green', 15);
+SELECT * FROM Bikes;
+
+
+
+#########################
+# Create Location Table #
+#########################
+CREATE TABLE Location (
+    id INT NOT NULL AUTO_INCREMENT,
+    Neighborhood VARCHAR(50),
+    City VARCHAR(50),
+    IDPerson INT NOT NULL,
+    FOREIGN KEY (IDPerson) REFERENCES People(PersonID),
+    PRIMARY KEY (id)
+);
+SHOW TABLES;
+DESCRIBE People;
+
+INSERT INTO Location(Neighborhood, City, IDPerson) VALUES ('Columbia Heights', 'Washington, DC', 3), ('Bushwick', 'Brooklyn', 6),('Shaw', 'Washington, DC', 9),('Red Hook', 'Brooklyn', 12),('East Village', 'Manhattan', 15);
+
+
+
+/* CREATE TABLE Subjects (
+   SubjectId INT NOT NULL AUTO_INCREMENT,
+   PRIMARY KEY (SubjectId))
+
+CREATE TABLE Students (
+   StudentId INT NOT NULL AUTO_INCREMENT,
+   PRIMARY KEY (StudentId))
+
+CREATE TABLE Entries(
+   EntriesId INT NOT NULL AUTO_INCREMENT,
+   SubjectId INT NOT NULL,
+   StudentId INT NOT NULL,
+   FOREIGN KEY (SubjectId) REFERENCES Subjects (SubjectId),
+   FOREIGN KEY (StudentId) REFERENCES Students (StudentId)) */
