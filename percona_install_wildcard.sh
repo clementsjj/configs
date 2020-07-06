6463c86c1080ffaf0309a62462bd8258b49eec5d
JJ Clements <clementsjj@gmail.com>
2020-07-06 15:06:03 -0400

    Update percona_install_wildcard.sh

--- percona_install_wildcard.sh
+++ percona_install_wildcard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+apt-get remove apparmor -y
 wget https://repo.percona.com/apt/percona-release_0.1-5.$(lsb_release -sc)_all.deb
 dpkg -i percona-release_0.1-5.$(lsb_release -sc)_all.deb
 apt-get update
@@ -7,3 +7,23 @@ apt-get upgrade
 apt-get dist-upgrade
 apt-get install percona-toolkit
 apt-get install percona-xtradb-cluster-server
+
+systemctl stop mysql
+
+cat > .tmux.conf<<EOF
+set -g mouse on
+EOF
+
+cat >>/etc/mysql/my.cnf<<EOF
+wsrep_provider=/usr/lib/libgalera_smm.so
+
+wsrep_cluster_name=democluster
+
+wsrep_cluster_address=gcomm://172.31.36.228,172.31.44.38,172.31.59.50
+
+wsrep_node_name=anne1
+wsrep_node_address=172.31.36.228
+
+wsrep_sst_auth=repuser:reppassword
+pxc_strict_mode=DISABLED
+EOF
